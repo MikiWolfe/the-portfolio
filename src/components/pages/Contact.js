75dd482fbb61143e4f8dfe8ab94c1ca7d08cf177
Mikayla Bruce <89227020+MikiWolfe@users.noreply.github.com>
2022-10-17 10:42:30 -0700

    style changes

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -16,23 +16,27 @@ export default function Contact() {
         <h3 className="contactHeader"> You can also find me on:</h3>
         
           <a className="contactIcon"
+          title="Twitter"
           href="https://twitter.com/MikiiWolf" 
           target="_blank">
             {<RiTwitterFill />}
           </a>
           <a className="contactIcon"
+          title="GitHub"
           href="https://github.com/MikiWolfe" 
           target="_blank">
             {<RiGithubFill />}
           </a>
 
           <a className="contactIcon"
+          title="Linked In"
             href="https://www.linkedin.com/in/mikayla-bruce-b50676221/"
             target="_blank"
           >
             {<RiLinkedinBoxFill />}
           </a>
           <a className="contactIcon"
+          title="Instagram"
           href="https://www.instagram.com/mikiwolfe/?hl=en" target="_blank">
             {<RiInstagramLine />}
           </a>
